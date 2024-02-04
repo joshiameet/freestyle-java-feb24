f70ddd3b5c17ba16fbefd61decb3ec314813f7f4
Ameet Joshi <103969058+joshiameet@users.noreply.github.com>
2024-02-04 15:23:36 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world from Ameet J!");
+        System.out.println("Hello, world from Ameet!");
 
     }
 
